8c4aaf1c5c0942457ae454456fa02ef73abe0f3a
drhard17 <maslak84@gmail.com>
2025-01-01 14:18:23 +0300

    add books with POST

--- src/main/java/booklib/BookLibraryApplication.java
+++ src/main/java/booklib/BookLibraryApplication.java
@@ -1,36 +1,12 @@
 package booklib;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import booklib.data.BookRepository;
-import booklib.model.Book;
 
 @SpringBootApplication
 public class BookLibraryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookLibraryApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner dataLoader(BookRepository bookRepo) {
-		return args -> {
-			
-			Book book1 = new Book();
-			book1.setTitle("T");
-			book1.setAuthor("Pelevin");
-			book1.setAvailable(true);
-			bookRepo.save(book1);
-
-			Book book2 = new Book();
-			book2.setTitle("Spring in action");
-			book2.setAuthor("Craig Walls");
-			book2.setAvailable(true);
-			bookRepo.save(book2);
-		};
-	}
-
+	
 }
--- src/main/java/booklib/api/BookController.java
+++ src/main/java/booklib/api/BookController.java
@@ -1,7 +1,10 @@
 package booklib.api;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +14,9 @@ import booklib.data.BookRepository;
 import booklib.model.Book;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -34,6 +40,25 @@ public class BookController {
     @ResponseStatus(HttpStatus.CREATED)
     public Book postBook(@RequestBody Book book) {
         return bookRepo.save(book);
+    }
+
+    @PutMapping(path = "/{id}", consumes = "application/json")
+    public Book updateBook(
+            @PathVariable("id") Long id,
+            @RequestBody Book book) {
+        book.setId(id);
+        return bookRepo.save(book);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    // @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<String> deleteBook(@PathVariable Long id) {
+        try {
+            bookRepo.deleteById(id);
+            return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
